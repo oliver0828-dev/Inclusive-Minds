6c900271804713156dc93ba298b2ff2918364149
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2024-01-03 23:06:29 +0900

    Create SchoolViewFramework.swift

---
+++ School/SchoolViewFramework.swift
@@ -0,0 +1,53 @@
+//
+//  SchoolViewFramework().swift
+//  Inclusive Minds
+//
+//  Created by Oliver Park on 12/16/23.
+//
+
+import SwiftUI
+
+struct SchoolViewFramework: View {
+    var framework: SchoolList
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(framework.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: UIScreen.main.bounds.width, height: 300)
+                .ignoresSafeArea(edges: .all)
+                .clipped()
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(framework.title)
+                            .font(.title)
+                        Text(framework.caption)
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                    }
+                    Spacer()
+                    Image(systemName: framework.icon)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                }
+                .padding([.horizontal, .top])
+                
+                Divider()
+                
+                ScrollView {
+                    Text(framework.description)
+                        .padding()
+                }
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+#Preview {
+    SchoolViewFramework(framework: schoolList.schoolData[0])
+}
