912e5d6516f4a816b34e62116dc01b77e8f0724f
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2024-01-04 10:06:16 +0900

    Create LMCRView.swift

---
+++ Cultural Racism/LMCRView.swift
@@ -0,0 +1,31 @@
+//
+//  LMCRView.swift
+//  Inclusive Minds
+//
+//  Created by Oliver Park on 11/22/23.
+//
+
+import SwiftUI
+
+struct LMCRView: View {
+    var body: some View {
+        
+        PopUpInfoView(titleName: "Negative Sides of Cultural Racism", description: """
+Stereotyping: Assigning negative or inaccurate traits to individuals based on their cultural background.
+
+Ethnocentrism: Believing that one's own culture is superior to all others, leading to a devaluation of other cultures.
+
+Invisibility: Ignoring or overlooking the contributions and perspectives of minority cultures.
+
+Cultural Appropriation: Misusing or exploiting elements of a culture without proper understanding or respect.
+
+Institutional Discrimination: Cultural biases embedded in policies, practices, and institutions that disadvantage minority groups.
+
+""")
+        
+    }
+}
+
+#Preview {
+    LMCRView()
+}
