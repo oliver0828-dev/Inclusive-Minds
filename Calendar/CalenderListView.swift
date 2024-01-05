ef626e0a8013c16e98be5503f216a7d5f9350a06
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2024-01-05 23:44:16 +0900

    Create CalenderListView.swift

---
+++ Calendar/CalenderListView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarListView.swift
+//  Inclusive Minds (2)
+//
+//  Created by Oliver Park on 1/5/24.
+//
+
+import SwiftUI
+
+struct CalendarListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarListView()
+}
