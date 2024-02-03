cd00984f9863e0d3fc1a7bd0426df54454dce733
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2024-02-03 11:57:59 +0900

    Create BadgeView.swift

---
+++ Quiz/BadgeView.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+struct BadgeView: View {
+    @State private var refreshToggle = false
+
+    var body: some View {
+        HStack(spacing: 70) {
+            ForEach(0..<3, id: \.self) { sectionID in
+                ZStack(alignment: .topLeading) {
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round, dash: [10, 5]))
+                        .frame(width: 150, height: 150)
+
+                    if BadgeManager.shared.hasBadge(forSection: sectionID) {
+                        Image("Badge\(sectionID + 1)")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 150, height: 150)
+                            .cornerRadius(15)
+                            .clipped()
+
+                        Button(action: {
+                            BadgeManager.shared.removeBadge(forSection: sectionID)
+                            refreshToggle.toggle()
+                        }) {
+                          Image(systemName: "xmark.circle.fill")
+                              .foregroundColor(.gray)
+                              .imageScale(.large)
+                        }
+                        .offset(x: -5, y: -5)
+                    }
+                }
+            }
+        }
+        .padding(.vertical, 70)
+        .id(refreshToggle)
+    }
+}
+
+// BadgeManager에 배지 삭제 메서드 추가
+extension BadgeManager {
+    func removeBadge(forSection sectionID: Int) {
+        UserDefaults.standard.set(false, forKey: "Badge\(sectionID)")
+    }
+}
